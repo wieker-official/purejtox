15414ff137905d86a84c00dc33cf1e3d7d7aba83
wieker <none@host.com>
2015-12-19 15:34:29 +0400

    PingArray stab

--- src/main/java/org/allesoft/purejtox/Ping.java
+++ src/main/java/org/allesoft/purejtox/Ping.java
@@ -14,17 +14,19 @@ import java.security.SecureRandom;
 public class Ping {
 
     DHT dht;
+    PingArray pingArray = new PingArray();
 
     public Ping(DHT dht) {
         this.dht = dht;
     }
 
     public void ping(IPPort ipPort, byte[] peerPublicKey) throws Exception {
-        byte[] ping_id = {1, 1, 1, 1, 1, 1, 1, 1, };
         byte[] pk = new byte[Const.DHT_PING_SIZE];
         byte[] ping_plain = new byte[Const.PING_PLAIN_SIZE];
 
         CryptoCore nacl = dht.getEncrypter(peerPublicKey);
+
+        byte[] ping_id = pingArray.add(peerPublicKey.clone());
 
         ping_plain[0] = 0;
         System.arraycopy(ping_id, 0, ping_plain, 1, ping_id.length);
